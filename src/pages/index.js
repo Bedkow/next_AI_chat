88b2aeb3596652fd0a9caf9394635b299551a770
Bedkow <bednarekdaniel@o2.pl>
2023-02-20 21:59:21 +0100

    d

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,18 +59,17 @@ export async function getServerSideProps() {
 
 	let maxTokens = 3600;
 	// let maxWords = 20;
-  let conversationTopic = "gun problem in the US";
+  let conversationTopic = "abortion";
   let data;
 
   try {
 	data = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `From now on keep context and roleplay 2 different people arguing back and forth about a topic, 1 argument at a time. They have to have opposing views at the beginning. Each person has to react to the other one's argument and create a counterargument. The topic is \"${conversationTopic}\". Put each person's arguments in separate responses and wrap them in html <div> elements with two unique classes of \"person1 argument1\", \"person1 argument2\". Do not change the editing of the <div> or classes throughout the conversation, write them in lower case only. Use the HTML standard of writing <div> and adding classes to it. Do not join person and argument classes.  Do not create any conclusion elements with other classes. Each person has to say minimum 2 arguments, maximum 3 arguments.\nThe pattern of the conversation should be:\n<div class=\"person1 argument1\">Person 1: Argument1</div>\n<div class=\"person2 argument1\">Person 2: Argument 1 based on Person 1's Argument 1</div>\n<div class=\"person1 argument2\">Person 1: Response to Person 2's Argument 1</div>\nEtc.\nConclude the conversation with the two agreeing on an agrument. Close all html <div> tags.`,
+    prompt: `From now on keep context and roleplay 2 different people arguing back and forth about a topic, 1 argument at a time. They have to have opposing views at the beginning. Each person has to react to the other one's argument and create a counterargument. The topic is \"${conversationTopic}\". Put each person's arguments in separate responses and wrap them in html <div> elements with two unique classes of \"person1 argument1\", \"person1 argument2\". Do not change the editing of the <div> or classes throughout the conversation, write them in lower case only. Use the HTML standard of writing <div> and adding classes to it. Do not join person and argument classes.  Do not create any conclusion elements with other classes. Each person has to say minimum 3 arguments, maximum 5 arguments.\nThe pattern of the conversation should be:\n<div class=\"person1 argument1\">Person 1: Argument1</div>\n<div class=\"person2 argument1\">Person 2: Argument 1 based on Person 1's Argument 1</div>\n<div class=\"person1 argument2\">Person 1: Response to Person 2's Argument 1</div>\nEtc.\nConclude the conversation with the two agreeing on an agrument. Close all html <div> tags.`,
     temperature: 0.2,
     max_tokens: maxTokens,
     top_p: 1,
-    frequency_penalty: 2,
-    presence_penalty: 0.2,
+	frequency_penalty: 0.2
   });
   } catch (error){
     console.log(error);
